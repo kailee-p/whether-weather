cbf0dbf3fc30629e5dd351402608ddb2c01f8f58
Kailee Pedersen <kaileepedersen@Kailees-Mac-mini.local>
2021-06-19 12:00:32 -0400

    cleaned up css

--- dist/src/index.js
+++ dist/src/index.js
@@ -7,7 +7,7 @@ const react_1 = __importDefault(require("react"));
 const react_dom_1 = __importDefault(require("react-dom"));
 const App_1 = __importDefault(require("./App"));
 const react_router_dom_1 = require("react-router-dom");
-require("./index.css");
+require("./css/index.css");
 react_dom_1.default.render(react_1.default.createElement(react_1.default.StrictMode, null,
     react_1.default.createElement(react_router_dom_1.BrowserRouter, null,
         react_1.default.createElement(App_1.default, null))), document.getElementById('root'));
